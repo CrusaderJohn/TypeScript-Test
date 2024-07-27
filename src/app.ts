657afd4e5269bc639d26fd356c5d6d63b1fb2928
John Jamieson <CrusaderJohn@JohnJamieson.ca>
2024-07-27 18:01:53 -0400

    Added light / dark switcher

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
-let message: string = 'Hello World';
-console.log(message);
+'use strict';
+
+const switcher: Element = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(): void {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className: string = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
